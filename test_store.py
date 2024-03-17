5d4bc8458dd6720052d7799f265e77723aa77b7b
Kam Nath <kmnath6@gmail.com>
2024-03-17 19:56:30 -0400

    new code

--- test_store.py
+++ test_store.py
@@ -1,6 +1,7 @@
 from jsonschema import validate
 import pytest
 import requests
+import uuid
 import schemas
 import api_helpers
 from hamcrest import assert_that, contains_string, is_
@@ -18,21 +19,23 @@ TODO: Finish this test by...
 
 @pytest.fixture
 def unique_order_data():
+    user_id = f"test_user_{uuid.uuid4().hex}"
     # You can create unique test data for each run here if needed
-    return {               
-        "order_id": "0771524a-56f9-415c-9bcc-328e7ca5f117", 
-        "pet_id": 0, 
-        "status" : "pending"         
+    return {   
+       #"order_id": "user_id",           
+       "order_id": "5822a554-edbd-40f9-a8f0-c183282a73a0", 
+       "pet_id": 0                 
     }
               
                 
            
 def test_patch_order_by_id(unique_order_data):
+    #url = f"http://localhost:5000/store/order/bea2599a-393e-4826-a0fd-edcc1ca92af3"
     url = f"http://127.0.0.1:5000/store/order/{unique_order_data['order_id']}"
-    headers = {"Content-Type": "application/json", "accept": "application/json"}   
+    headers = { "accept": "application/json", "Content-Type": "application/json"}   
 
     payload = {
-        "status": unique_order_data["status"],
+         "status" : "sold"         
         
     }
    
@@ -42,3 +45,6 @@ def test_patch_order_by_id(unique_order_data):
    
     expected_message = "Order and pet status updated successfully"
     assert response.json()["message"] == expected_message
+
+
+
